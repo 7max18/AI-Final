064275ee9ddacb602f02a5c4b0560025a95c7bf1
7max18 <maxkoppel0703@gmail.com>
2021-11-25 21:07:10 -0500

    Added crates the player can hide in

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private float moveDirectionLR = 0;
     private float moveDirectionUD = 0;
     private Rigidbody rb;
+    private GameObject hidingSpot;
+    private bool inHidingRange = false;
+    private bool hiding = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,31 +20,51 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movement controls
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if(!hiding)
         {
-            moveDirectionLR = -1;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            moveDirectionLR = 1;
-        }
-        else
-        {
-            moveDirectionLR = 0;
+            // Movement controls
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                moveDirectionLR = -1;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                moveDirectionLR = 1;
+            }
+            else
+            {
+                moveDirectionLR = 0;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                moveDirectionUD = -1;
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                moveDirectionUD = 1;
+            }
+            else
+            {
+                moveDirectionUD = 0;
+            }
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            moveDirectionUD = -1;
-        }
-        else if (Input.GetKey(KeyCode.UpArrow))
-        {
-            moveDirectionUD = 1;
-        }
-        else
-        {
-            moveDirectionUD = 0;
+            if (hidingSpot != null)
+            {
+                if (!hiding)
+                {
+                    transform.position = hidingSpot.GetComponent<Renderer>().bounds.center;
+                }
+                else
+                {
+                    transform.position = hidingSpot.GetComponent<Renderer>().bounds.center + hidingSpot.transform.forward;
+                }
+
+                hiding = !hiding;
+            }
         }
 
         Camera.main.transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
@@ -50,5 +73,21 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(moveDirectionLR * maxSpeed, 0, moveDirectionUD * maxSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Hiding Spot"))
+        {
+            hidingSpot = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hiding Spot"))
+        {
+            hidingSpot = null;
+        }
     }
 }
