c3ff61447c19351191df98025d4ea0a52e1d3b94
7max18 <maxkoppel0703@gmail.com>
2021-11-25 20:38:57 -0500

    Added test scene and player placeholder

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 3.0f;
+    private float moveDirectionLR = 0;
+    private float moveDirectionUD = 0;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement controls
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveDirectionLR = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveDirectionLR = 1;
+        }
+        else
+        {
+            moveDirectionLR = 0;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveDirectionUD = -1;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveDirectionUD = 1;
+        }
+        else
+        {
+            moveDirectionUD = 0;
+        }
+
+        Camera.main.transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveDirectionLR * maxSpeed, 0, moveDirectionUD * maxSpeed);
+    }
+}
