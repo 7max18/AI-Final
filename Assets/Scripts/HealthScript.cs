f4ba6e2d120d917dd57fa41f96a9d46b79b6410f
Sebastian Sandrin <56841231+ssandrin2018@users.noreply.github.com>
2021-11-29 17:05:42 -0500

    Improved the HealthScript
    Improved the HealthScript. It first checks if the script is attached to player, since that influences how the script will behave. When the object the script is attached to runs out of health, it takes the appropriate action depending on if "isPlayer" is true or false. Finally, it updates a healthbar, but only if the script is attached to the player.

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -1,13 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthScript : MonoBehaviour
 {
     public float health;
 
+    public Slider healthbar;
+
+    private bool isPlayer;
+
+    private void Start()
+    {
+        if (gameObject.tag == "Player") { isPlayer = true; }
+        else { isPlayer = false; }
+    }
+
     public void damage (float attack)
     {
         health -= attack;
+        if (health <= 0)
+        {
+            //Trigger the death animation
+
+            if (isPlayer == true)
+            {
+                //Broadcast a message to the rest of the scene that the player died
+                //Reset the scene after a wait (maybe use an IEnumerator?)
+            }
+            else
+            {
+                //Destroy the character
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayer == true) { healthbar.value = health; }        
     }
 }
