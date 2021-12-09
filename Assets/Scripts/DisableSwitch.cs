3aab7f30b80ec0e0b58d5f3b2ae10be58749cad7
Sebastian Sandrin <56841231+ssandrin2018@users.noreply.github.com>
2021-12-09 17:55:32 -0500

    Disable Switch code

---
+++ Assets/Scripts/DisableSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableSwitch : MonoBehaviour
+{
+    //IMPORTANT!! Remember to set the object that you want to be disabled when the player gets near.
+    //You should *only pair a Search Tower or Drone Guard.*
+    public GameObject pairedObject;
+
+    private void Start()
+    {
+        if (pairedObject = null)
+        {
+            Debug.LogError("Error: Please set an object to disable with the switch!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pairedObject.SetActive(false);
+        }
+    }
+}
